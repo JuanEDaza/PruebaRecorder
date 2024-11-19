700bbb40aa5ec181817cb0d83a7be7b98d611cf3
JuanEDaza <80993686+JuanEDaza@users.noreply.github.com>
2024-11-19 14:20:21 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
 
-var gumStream; 						//stream from getUserMedia()
-var recorder; 						//WebAudioRecorder object
-var input; 							//MediaStreamAudioSourceNode  we'll be recording
-var encodingType; 					//holds selected encoding for resulting audio (file)
-var encodeAfterRecord = true;       // when to encode
+var gumStream; 				//stream from getUserMedia()
+var recorder; 				//WebAudioRecorder object
+var input; 						//MediaStreamAudioSourceNode  we'll be recording
+var encodingType; 		//holds selected encoding for resulting audio (file)
+var encodeAfterRecord = true; 	// when to encode
 var dynamicBlob;
 let timer;
 let seconds = 0;
@@ -18,6 +18,7 @@ var encodingTypeSelect = document.getElementById("encodingTypeSelect");
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 var sendButton = document.getElementById("sendButton");
+const timerDisplay = document.getElementById('timer');
 
 //add events to those 3 buttons
 recordButton.addEventListener("click", startRecording);
@@ -26,44 +27,16 @@ sendButton.addEventListener('click', sendMessage);
 
 function startRecording() {
 	console.log("startRecording() called");
-
-	/*
-		Simple constraints object, for more advanced features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-
 	var constraints = { audio: true, video: false }
-	/*
-		We're using the standard promise based getUserMedia() 
-		https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-*/
-
 	navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-		/* __log("getUserMedia() success, stream created, initializing WebAudioRecorder..."); */
-
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-
-		*/
 		audioContext = new AudioContext();
-
-		//update the format 
-		/* document.getElementById("formats").innerHTML = "Format: 2 channel " + encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value + " @ " + audioContext.sampleRate / 1000 + "kHz" */ // SE PUEDE BORRAR, INNECESARIO
-
 		//assign to gumStream for later use
 		gumStream = stream;
-
 		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
 
-		//stop the input from playing back through the speakers
-		//input.connect(audioContext.destination)
-
 		//get the encoding 
 		encodingType = encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value; // SE PUEDE MODIFICAR Y SIMPLEMENTE PONER mp3
-
 		//disable the encoding selector
 		encodingTypeSelect.disabled = true; // SE PUEDE BORRAR, INNECESARIO
 
@@ -71,36 +44,21 @@ function startRecording() {
 			workerDir: "js/", // must end with slash
 			encoding: encodingType,
 			numChannels: 2, //2 is the default, mp3 encoding supports only 2
-			onEncoderLoading: function (recorder, encoding) {
-				// show "loading encoder..." display
-				/* __log("Loading " + encoding + " encoder..."); */
-			},
-			onEncoderLoaded: function (recorder, encoding) {
-				// hide "loading encoder..." display
-				/* __log(encoding + " encoder loaded"); */
-			}
 		});
-
 		recorder.onComplete = function (recorder, blob) {
-			/* __log("Encoding complete"); */
 			createDownloadLink(blob, recorder.encoding);
 			console.warn(blob);
 			dynamicBlob = blob;
-			/* encodingTypeSelect.disabled = false; */ // SE PUEDE BORRAR, INNECESARIO
 		}
-
 		recorder.setOptions({
 			timeLimit: 120,
 			encodeAfterRecord: encodeAfterRecord,
 			ogg: { quality: 0.5 },
 			mp3: { bitRate: 160 }
 		});
-
 		//start the recording process
 		recorder.startRecording();
 		startTimer();
-
-		/* __log("Recording started"); */
 
 	}).catch(function (err) {
 		//enable the record button if getUSerMedia() fails
@@ -113,7 +71,8 @@ function startRecording() {
 	//disable the record button
 	recordButton.disabled = true;
 	stopButton.disabled = false;
-	/* sendButton.disabled = false; */
+	recordingsList.innerHTML = "";
+	sendButton.style.display = "none";
 }
 
 function stopRecording() {
@@ -122,16 +81,15 @@ function stopRecording() {
 	//stop microphone access
 	gumStream.getAudioTracks()[0].stop();
 
-	//disable the stop button
+	//disable the buttons
 	stopButton.disabled = true;
 	recordButton.disabled = false;
 	sendButton.disabled = false;
+	sendButton.style.display = "block";
 
 	//tell the recorder to finish the recording (stop recording + encode the recorded audio)
 	recorder.finishRecording();
 	stopTimer();
-
-	/* __log('Recording stopped'); */
 }
 
 // Función modificada para evitar crear una lista y reemplazar el elemento existente
@@ -143,33 +101,29 @@ function createDownloadLink(blob, encoding) {
 	if (!existingAudio || !existingLink) {
 		// Si no existen, crea los elementos
 		var au = document.createElement('audio');
-		var link = document.createElement('a');
+		/* var link = document.createElement('a'); */
 
 		// Configura el <audio> y el enlace
 		au.controls = true;
 		au.src = url;
 
-		link.href = url;
+		/* link.href = url;
 		link.download = new Date().toISOString() + '.' + encoding;
-		link.innerHTML = link.download;
+		link.innerHTML = link.download; */
 
 		// Agrega los elementos al contenedor
 		recordingsList.innerHTML = ""; // Limpia cualquier contenido previo
 		recordingsList.appendChild(au);
-		recordingsList.appendChild(link);
+		/* recordingsList.appendChild(link); */
 	} else {
 		// Si ya existen, actualiza sus propiedades
 		existingAudio.src = url;
-		existingLink.href = url;
+		/* existingLink.href = url;
 		existingLink.download = new Date().toISOString() + '.' + encoding;
-		existingLink.innerHTML = existingLink.download;
+		existingLink.innerHTML = existingLink.download; */
 	}
 }
 
-//helper function
-/* function __log(e, data) {
-	log.innerHTML += "\n" + e + " " + (data || '');
-} */
 
 function startTimer() {
 	timer = setInterval(() => {
